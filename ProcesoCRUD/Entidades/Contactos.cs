3e0e128635119c40708b4c6d2a3924629cc9d9a8
ShootDomy <canizaresdomenica4@gmail.com>
2025-07-30 15:09:42 -0500

    feat: Agregar entidad de contactos

---
+++ ProcesoCRUD/Entidades/Contactos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesoCRUD.Entidades
+{
+    public class Contactos
+    {
+        public string Con_uuid { get; set; }
+        public string Con_nombre { get; set; }
+        public string Con_telefono { get; set; }
+        public string Con_correo { get; set; }
+        public DateTime Con_fecha_nac { get; set; }
+        public string Car_uuid { get; set; }
+        public Boolean Con_activo { get; set; }
+
+    }
+}
